54d36d82e034bff2d8da6ce4a51e3a12066dae4c
gamesindian <96935284+gamesindian@users.noreply.github.com>
2023-01-20 12:09:52 +0530

    excel added

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ function tableband() {
     .forEach(function (e) {
       e.remove();
     });
+  // display: none;
+
 }
 function toggleOverlay(elem) {
   if (elem.getAttribute("data-active") == "1") {
@@ -75,79 +77,19 @@ function tablemaxi() {
   document.getElementById("block_xyz").style.height = 250 + "px";
 }
 
-function tableToCSV() {
-  // Variable to store the final csv data
-  var csv_data = [];
+//download table as excel
+function downloadTableAsExcel() {
+    var iframe = document.getElementById("block_xyz");
+    var html = iframe.contentWindow.document.getElementById("adstable").outerHTML;
+    var url = "data:application/vnd.ms-excel," + escape(html); // Set your html table into url
+    location.href = url;
+    return false;
 
-  // Get each row data
-  var rows = document.getElementsByTagName("tr");
-  for (var i = 0; i < rows.length; i++) {
-    // Get each column data
-    var cols = rows[i].querySelectorAll("td,th");
-
-    // Stores each csv row data
-    var csvrow = [];
-    for (var j = 0; j < cols.length; j++) {
-      // Get the text data of each cell
-      // of a row and push it to csvrow
-      csvrow.push(cols[j].innerHTML);
-    }
-
-    // Combine each column value with comma
-    csv_data.push(csvrow.join(","));
-  }
-
-  // Combine each row data with new line character
-  csv_data = csv_data.join("\n");
-
-  // Call this function to download csv file
-  downloadCSVFile(csv_data);
 }
 
-var tableToExcel = (function () {
-  var uri = "data:application/vnd.ms-excel;base64,",
-    template =
-      '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>',
-    base64 = function (s) {
-      return window.btoa(unescape(encodeURIComponent(s)));
-    },
-    format = function (s, c) {
-      return s.replace(/{(\w+)}/g, function (m, p) {
-        return c[p];
-      });
-    };
-  return function (table, name) {
-    if (!table.nodeType) table = document.getElementById(table);
-    var ctx = { worksheet: name || "Worksheet", table: table.innerHTML };
-    window.location.href = uri + base64(format(template, ctx));
-  };
-})();
 
-function downloadCSVFile(csv_data) {
-  // Create CSV file object and feed
-  // our csv_data into it
-  CSVFile = new Blob([csv_data], {
-    type: "text/csv",
-  });
 
-  // Create to temporary link to initiate
-  // download process
-  var temp_link = document.createElement("a");
 
-  // Download csv file
-  temp_link.download = "GfG.csv";
-  var url = window.URL.createObjectURL(CSVFile);
-  temp_link.href = url;
-
-  // This link should not be displayed
-  temp_link.style.display = "none";
-  document.body.appendChild(temp_link);
-
-  // Automatically click the link to
-  // trigger download
-  temp_link.click();
-  document.body.removeChild(temp_link);
-}
 
 function refreshfunction() {
   wholescript();
@@ -158,6 +100,7 @@ function wholescript() {
     var iDiv = document.createElement("iframe");
     iDiv.id = "block_xyz";
     iDiv.className = "popup";
+    iDiv.style.width = "100%";
     document.getElementsByTagName("body")[0].appendChild(iDiv);
 
     function getSlotTargetValues(obj, keys) {
@@ -298,7 +241,7 @@ function wholescript() {
       }
 
       var html =
-        "<div class='control-buttons'><button id='close_table'class='ctable' onclick='parent.tableband();'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-x-circle-fill' viewBox='0 0 16 16'><path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z'/></svg></button><button id='mini_table'class='minitable' onclick='parent.tablemini();'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-fullscreen-exit' viewBox='0 0 16 16'><path d='M5.5 0a.5.5 0 0 1 .5.5v4A1.5 1.5 0 0 1 4.5 6h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5zm5 0a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 10 4.5v-4a.5.5 0 0 1 .5-.5zM0 10.5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 6 11.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zm10 1a1.5 1.5 0 0 1 1.5-1.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4z'/></svg></button><button id='maxi_table'class='dtable' onclick='parent.tablemaxi();'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-fullscreen' viewBox='0 0 16 16'><path d='M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1h-4zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zM.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5z'/></svg></button><button id='downloadcsv' type='button' onclick='parent.tableToExcel('adstable', 'W3C Example Table')'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-cloud-arrow-down-fill' viewBox='0 0 16 16'><path d='M8 2a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 6.095 0 7.555 0 9.318 0 11.366 1.708 13 3.781 13h8.906C14.502 13 16 11.57 16 9.773c0-1.636-1.242-2.969-2.834-3.194C12.923 3.999 10.69 2 8 2zm2.354 6.854-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 9.293V5.5a.5.5 0 0 1 1 0v3.793l1.146-1.147a.5.5 0 0 1 .708.708z'/></svg></button><button onclick='parent.refreshfunction();' id='refreshid'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-arrow-clockwise' viewBox='0 0 16 16'><path fill-rule='evenodd' d='M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z'/><path d='M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z'/></svg></button><button data-active='1' onclick='parent.toggleOverlay(this)' id='eye_close_overlay' class='eye_close_overlay_btn'>Hide</button></div><div id='adstable' class='table-responsive'><table class='table table-bordered table-dark'><thead><tr>" +
+        "<div class='control-buttons'><button id='close_table'class='ctable' onclick='parent.tableband();'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-x-circle-fill' viewBox='0 0 16 16'><path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z'/></svg></button><button id='mini_table'class='minitable' onclick='parent.tablemini();'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-fullscreen-exit' viewBox='0 0 16 16'><path d='M5.5 0a.5.5 0 0 1 .5.5v4A1.5 1.5 0 0 1 4.5 6h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5zm5 0a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 10 4.5v-4a.5.5 0 0 1 .5-.5zM0 10.5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 6 11.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zm10 1a1.5 1.5 0 0 1 1.5-1.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4z'/></svg></button><button id='maxi_table'class='dtable' onclick='parent.tablemaxi();'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-fullscreen' viewBox='0 0 16 16'><path d='M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1h-4zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zM.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5z'/></svg></button><button id='downloadcsv' type='button' onclick='parent.downloadTableAsExcel()'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-cloud-arrow-down-fill' viewBox='0 0 16 16'><path d='M8 2a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 6.095 0 7.555 0 9.318 0 11.366 1.708 13 3.781 13h8.906C14.502 13 16 11.57 16 9.773c0-1.636-1.242-2.969-2.834-3.194C12.923 3.999 10.69 2 8 2zm2.354 6.854-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 9.293V5.5a.5.5 0 0 1 1 0v3.793l1.146-1.147a.5.5 0 0 1 .708.708z'/></svg></button><button onclick='parent.refreshfunction();' id='refreshid'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-arrow-clockwise' viewBox='0 0 16 16'><path fill-rule='evenodd' d='M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z'/><path d='M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z'/></svg></button><button data-active='1' onclick='parent.toggleOverlay(this)' id='eye_close_overlay' class='eye_close_overlay_btn'>Hide</button></div><div id='adstable' class='table-responsive'><table class='table table-bordered table-dark'><thead><tr>" +
         cols
           .map(function (c) {
             return "<th scope='col'>" + c + "</th>";
